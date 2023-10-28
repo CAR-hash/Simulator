841350ac054329fabd849ad73847b0a27d514638
CAR-hash <lejian663@126.com>
2023-10-28 11:20:57 +0800

    debug

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import simpy
 import numpy as np
 import requests
 import json
+import matplotlib.pyplot as plt
 
 class PkgLRUCache(object):
     def __init__(self, capacity):
@@ -19,6 +20,8 @@ class PkgLRUCache(object):
     # else return false
     # update cache after that
     def update(self, pkg):
+        if pkg[0] == -1:
+            return True
         if pkg[1] > self.capacity:
             return False
         if pkg[0] not in self.queue:
@@ -28,6 +31,8 @@ class PkgLRUCache(object):
             else:
                 while self.size > self.capacity:
                     v = self.queue.popitem(last=False)
+                    if type(v[1]) != float:
+                        v = v[1]
                     self.size -= v[1]
                 self.queue[pkg[0]] = pkg
             return False
@@ -73,18 +78,45 @@ class RandomRequest(Request):
 class Statistician(object):
     def __init__(self, worker_count = 1000):
         self.global_hit_count = [
+            [],[],[]
         ]
-        for i in range(0, worker_count):
-            self.global_hit_count.append(0)
+        for mode in range(0, 3):
+            for i in range(0, worker_count):
+                self.global_hit_count[mode].append(0)
         self.global_total_count = [
-
+            [],[],[]
         ]
-        for i in range(0, worker_count):
-            self.global_total_count.append(0)
+        for mode in range(0, 3):
+            for i in range(0, worker_count):
+                self.global_total_count[mode].append(0)
 
+        #
+        self.active_tasks = [0, 0, 0]
+
+    def show(self):
+        hit = [[], [], []]
+        for mode in range(0, 3):
+            for i in range(0, 1000):
+                if self.global_total_count[mode][i] == 0:
+                    hit[mode].append(50)
+                else:
+                    hit[mode].append(
+                        100 * self.global_hit_count[mode][i] / self.global_total_count[mode][i])
+
+        labels = ["Least loaded", "Hash Affinity", "PASch"]
+        fig, axs = plt.subplots()
+        for i in range(0, 1000):
+            # rectangular box plot
+            bplot = axs.boxplot(hit,
+                                vert=True,  # vertical box alignment
+                                patch_artist=True,  # fill with color
+                                labels=labels)  # will be used to label x-ticks
+            axs.set_xlabel("Scheduling Algorithm")
+            axs.set_ylabel("Hit rate(%)")
+        plt.show()
 
 class Worker(object):
-    def __init__(self, sta: Statistician, env: simpy.Environment, w_id: int, assign_task: simpy.Event):
+    def __init__(self, sta: Statistician, mode, env: simpy.Environment, w_id: int, assign_task: simpy.Event):
         self.w_id = w_id
         self.env = env
         self.workload = []
@@ -93,7 +125,7 @@ class Worker(object):
         self.st = 100
         self.cache = PkgLRUCache(capacity=500)
         self.sta = sta
-
+        self.mode = mode
         self.local_hit_count = 0
         self.local_total_count = 0
 
@@ -101,54 +133,87 @@ class Worker(object):
         while True:
             if len(self.workload) > 0:
                 task = self.workload.pop(0)
-                hit = 0
-                total = len(task.pkgs)
-                for pkg in task.pkgs:
-                    if self.cache.update(pkg):
-                        hit = hit + 1
+                #hit = 0
+                #total = len(task.pkgs)
+                #for pkg in task.pkgs:
+                #    if self.cache.update(pkg):
+                #        hit = hit + 1
+                self.sta.global_total_count[self.mode][self.w_id] = self.sta.global_total_count[self.mode][
+                                                                        self.w_id] + 1
+                if self.cache.update(task.max_pkg):
+                    self.sta.global_hit_count[self.mode][self.w_id] = self.sta.global_hit_count[self.mode][self.w_id] + 1
+                    turn_around = 1000 + task.execution
+                else:
+                    turn_around = np.random.default_rng().exponential(scale=4007) + 1000 + task.execution
 
-                self.sta.global_hit_count[self.w_id] = self.sta.global_hit_count[self.w_id] + hit
-                self.sta.global_total_count[self.w_id] = self.sta.global_total_count[self.w_id] + total
-
-                turn_around = (total-hit)*np.random.default_rng().exponential(scale=4007) + 1000 + task.execution
                 yield self.env.timeout(turn_around)
+                self.sta.active_tasks[self.mode] = max(0, self.sta.active_tasks[self.mode]-1)
             else:
                 yield self.assign_task
 
 class Cloud(object):
-    def __init__(self, env, sta, worker_count):
+    def __init__(self, env, sta, worker_count, mode="Least loaded"):
         self.env: simpy.Environment = env
         self.assign_task = []
 
         self.sta = sta
 
+        self.mode = mode
+        self.c = 1.25
+
         for i in range(worker_count):
             self.assign_task.append(self.env.event())
         self.workers = []
+
+        self.n_mode = 0
+        if mode == "Hash Affinity":
+            self.n_mode = 1
+        elif mode == "PASch":
+            self.n_mode = 2
         for i in range(worker_count):
-            self.workers.append(Worker(sta=self.sta, env=env, w_id=i, assign_task=self.assign_task[i]))
+            self.workers.append(Worker(sta=self.sta, mode=self.n_mode, env=env, w_id=i, assign_task=self.assign_task[i]))
 
     def handle_request(self, req: Request):
-        #worker = np.random.default_rng().choice(self.workers)
+        self.sta.active_tasks[self.n_mode] = self.sta.active_tasks[self.n_mode] + 1
 
-        w_1 = self.__m(req.max_pkg[0])
-        w_2 = self.__m(req.max_pkg[0] + 10)
-
-        w_id = w_1
-        if len(self.workers[w_2].workload) < len(self.workers[w_1].workload):
-            w_id = w_2
-
-        if len(self.workers[w_id].workload) > self.workers[w_id].st:
+        if self.mode == "Least loaded":
+            w_id = 0
             for worker in self.workers:
                 if len(worker.workload) < len(self.workers[w_id].workload):
                     w_id = worker.w_id
-
-        #w_id = self.__m(req.max_pkg)
-
-        self.__distribute_task_to(w_id, req)
+            self.__distribute_task_to(w_id, req)
+        elif self.mode == "Hash Affinity":
+            w_id = self.__m(req.max_pkg[0])
+            self.__distribute_task_to(w_id, req)
+        elif self.mode == "PASch":
+            w_1 = self.__m(req.max_pkg[0])
+            w_2 = self.__m(req.max_pkg[0] + 10)
+            w_id = w_1
+            if len(self.workers[w_2].workload) < len(self.workers[w_1].workload):
+                w_id = w_2
+            if len(self.workers[w_id].workload) > self.workers[w_id].st:
+                for worker in self.workers:
+                    if len(worker.workload) < len(self.workers[w_id].workload):
+                        w_id = worker.w_id
+            self.__distribute_task_to(w_id, req)
 
     def __m(self, pkg_id):
         return pkg_id % 1000
+        idx = pkg_id % 1000
+        first_search = True
+        while True:
+            if len(self.workers[idx].workload) < self.sta.active_tasks[self.n_mode]/800:
+                break
+            else:
+                idx = idx + 1
+                if idx >= 1000:
+                    idx = 0
+                    if first_search:
+                        first_search = False
+                    else:
+                        idx = np.random.default_rng().choice(self.workers).w_id
+                        break
+        return idx
 
     def __distribute_task_to(self, worker_id, req):
         self.assign_task[worker_id].succeed()
@@ -158,8 +223,9 @@ class Cloud(object):
 
 
 class RequestGenerator(object):
-    def __init__(self, env: simpy.Environment, cloud: Cloud, period):
+    def __init__(self, sta,env: simpy.Environment, cloud: list[Cloud], period):
         self.env = env
+        self.sta = sta
         self.cloud = cloud
         self.period = period
         self.action = self.env.process(self.request_generator())
@@ -174,8 +240,11 @@ class RequestGenerator(object):
         while True:
             try:
                 yield self.env.timeout(np.random.default_rng().exponential(scale=0.1))
-                self.cloud.handle_request(RandomRequest(sampler=self.sampler))
+                self.cloud[1].handle_request(RandomRequest(sampler=self.sampler))
+                for c in self.cloud:
+                    c.handle_request(RandomRequest(sampler=self.sampler))
             except simpy.Interrupt:
+                self.sta.show()
                 print("now:{}, time to sleep...".format(self.env.now))
                 break
 
@@ -185,14 +254,14 @@ def print_hi(name):
 
     statistician = Statistician()
 
-    cloud = Cloud(env=env, sta=statistician, worker_count=1000)
-    generator = RequestGenerator(env=env, cloud=cloud, period=10000)
+    cloud = Cloud(env=env, sta=statistician, worker_count=1000,mode="Least loaded")
+    cloud2 = Cloud(env=env, sta=statistician, worker_count=1000,mode="Hash Affinity")
+    cloud3 = Cloud(env=env, sta=statistician, worker_count=1000,mode="PASch")
+    generator = RequestGenerator(env=env, sta=statistician, cloud=[cloud, cloud2, cloud3], period=500)
 
     env.run()
 
-    for i in range(1000):
-        if statistician.global_total_count[i]!=0:
-            print(statistician.global_hit_count[i]/statistician.global_total_count[i])
+
 
 
 # Press the green button in the gutter to run the script.
